3a576e5d8ec4da1136aa55fa35f7dee7e703a8ae
Sumit <akoliyasumit@hotmail.com>
2022-02-10 23:40:29 +0530

    User info validation and save

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const User = require(`../models/User`)
+const { registerValidation } = require('../validation/userValidation')
+const { loginValidation } = require('../validation/userValidation')
+
+//Validation
 
 router.post('/register', async (req,res)=>{
-    console.log(`name:` ,req.body.name)
+    // console.log(`name:` ,req.body.name)
+    //validate the data before we put into db
+    const {error} = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    
+    //checking if the user is already in the database
+    const emailExist = await User.findOne({email:req.body.email})
+    if(emailExist) return res.status(400).send('Email already exists')
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
